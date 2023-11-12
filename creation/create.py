6b6c0a3223cf8a3892ca587fb9eb332a7c9f2030
Dima Nesterenko <nesterenkoodmytro@gmail.com>
2023-11-12 21:25:11 +0200

    Add methods

--- creation/create.py
+++ creation/create.py
@@ -1,5 +1,5 @@
 from owlready2 import *
-
+from rdflib import Graph
 onto = get_ontology("social_network.owl")
 
 with onto:
@@ -43,3 +43,53 @@ mark.age = 20
 sasha.age=25
 
 onto.save(file = "social_network.owl", format = "ntriples")
+
+#16_methods
+def calculate_average_age_in_group(group):
+    if isinstance(group, Group):
+        total_age = 0
+        total_persons = 0
+        for person in group.memberOf:
+            if isinstance(person, Person):
+                total_age += person.age
+                total_persons += 1
+        if total_persons > 0:
+            average_age = total_age / total_persons
+            return f"Average age in {group.name}: {average_age}"
+        else:
+            return f"No persons in {group.name}"
+    else:
+        return "Invalid input, expecting a Group instance"
+
+
+print(calculate_average_age_in_group(groupPython))
+
+
+def find_oldest_person(ontology):
+    max_age = 0
+    oldest_person = None
+    for person in ontology.Person.instances():
+        if person.age > max_age:
+            max_age = person.age
+            oldest_person = person
+    if oldest_person:
+        return f"The oldest person is {oldest_person.name} with age {max_age}"
+    else:
+        return "No persons in the network"
+
+print(find_oldest_person(onto))
+
+
+def are_persons_friends(person1, person2):
+    if isinstance(person1, Person) and isinstance(person2, Person):
+        return f"{person1.name} and {person2.name} are friends: {person2 in person1.hasFriend}"
+    else:
+        return "Invalid input, expecting Person instances"
+
+print(are_persons_friends(dima, sasha))
+
+
+
+
+#15_queries
+
